7e1bb7466ebbb16f4e75b3c33a7fab92316bb377
kuqin <kuqin@microsoft.com>
2022-03-01 12:40:40 -0800

    StandaloneMmPkg/StandaloneMmMemLib: Communicate Buffer Validation Function
    Current MMI handlers would leverage MmIsBufferOutsideMmValid interface
    to validate the incoming communicate buffer.
    
    However, if/when MM core would like to improve security level by copying
    the communicate buffer into TSEG before dispatching, these handlers will
    fail due to these changes.
    
    This change added new interface for traditional MM handlers to validate
    incoming communicate buffer, which should pave way for MM core to copy
    incoming buffer into TSEG prior to handler dispatching. The current
    implementation will remain to use MmIsBufferOutsideMmValid till Core has
    such functionality.
    
    The interface should be used and only used for validating communicate
    buffer upon dispatching. For other memory region validity check, handlers
    should still use MmIsBufferOutsideMmValid.

--- StandaloneMmPkg/Include/Library/StandaloneMmMemLib.h
+++ StandaloneMmPkg/Include/Library/StandaloneMmMemLib.h
@@ -12,7 +12,8 @@
 
 **/
 
-#pragma once
+#ifndef _MM_MEM_LIB_H_
+#define _MM_MEM_LIB_H_
 
 /**
   This function check if the buffer is valid per processor architecture and not overlap with MMRAM.
@@ -129,3 +130,23 @@ MmSetMem (
   IN UINTN  Length,
   IN UINT8  Value
   );
+
+/** MU_CHANGE Start: Add Communication Buffer Validation
+  This function should be used for MMI handlers to check if the communicate buffer is valid.
+
+  @param Buffer  The buffer start address to be checked.
+  @param Length  The buffer length to be checked.
+
+  @retval TRUE  This communicate buffer is valid per processor architecture.
+  @retval FALSE This communicate buffer is not valid per processor architecture.
+**/
+BOOLEAN
+EFIAPI
+MmCommBufferValid (
+  IN EFI_PHYSICAL_ADDRESS  Buffer,
+  IN UINT64                Length
+  );
+
+// MU_CHANGE End: Add Communication Buffer Validation
+
+#endif
