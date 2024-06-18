c4336b6eeea008dabddbe7a0be27beb468fc5e00
Michael Kubacki <michael.kubacki@microsoft.com>
2024-06-18 15:20:37 -0400

    BaseCore: Enable CI
    The minimal changes to get most feature agnostic CI enabled and
    similar to the results that would have been produced in the
    release/202502 branch.
    
    In some cases, minor bug fixes or other changes happened in build
    and/or CI plugins that will be addressed when those plugins are
    individually updated in this branch. The most critical changes to
    enable base functionality are included.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>
    
    ---
    
    This PR was created automatically by the
    [repo-file-sync-action](https://github.com/BetaHuhn/repo-file-sync-action)
    workflow run
    [#13187781564](https://github.com/microsoft/mu_devops/actions/runs/13187781564)
    
    Signed-off-by: Project Mu UEFI Bot <uefibot@microsoft.com>

--- .pytool/CISettings.py
+++ .pytool/CISettings.py
@@ -7,30 +7,18 @@
 ##
 import os
 import logging
-import sys
 from edk2toolext.environment import shell_environment
 from edk2toolext.invocables.edk2_ci_build import CiBuildSettingsManager
+from edk2toolext.invocables.edk2_parse import ParseSettingsManager
+from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
 from edk2toolext.invocables.edk2_setup import SetupSettingsManager, RequiredSubmodule
 from edk2toolext.invocables.edk2_update import UpdateSettingsManager
-from edk2toolext.invocables.edk2_pr_eval import PrEvalSettingsManager
+
 from edk2toollib.utility_functions import GetHostInfo
-from pathlib import Path
 
+from edk2toolext import codeql as codeql_helpers
 
-try:
-    # Temporarily needed until edk2 can update to the latest edk2-pytools
-    # that has the CodeQL helpers.
-    #
-    # May not be present until submodules are populated.
-    #
-    root = Path(__file__).parent.parent.resolve()
-    sys.path.append(str(root/'BaseTools'/'Plugin'/'CodeQL'/'integration'))
-    import stuart_codeql as codeql_helpers
-except ImportError:
-    pass
-
-
-class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManager, PrEvalSettingsManager):
+class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManager, PrEvalSettingsManager, ParseSettingsManager):
 
     def __init__(self):
         self.ActualPackages = []
@@ -51,7 +39,6 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
 
     def RetrieveCommandLineOptions(self, args):
         super().RetrieveCommandLineOptions(args)
-
         try:
             self.codeql = codeql_helpers.is_codeql_enabled_on_command_line(args)
         except NameError:
@@ -65,6 +52,7 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
         ''' return iterable of edk2 packages supported by this build.
         These should be edk2 workspace relative paths '''
 
+<<<<<<< HEAD
         return ("ArmPkg",
                 "ArmPlatformPkg",
                 "ArmVirtPkg",
@@ -74,15 +62,24 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
                 "IntelFsp2Pkg",
                 "IntelFsp2WrapperPkg",
                 "ManageabilityPkg",
+=======
+        return ("BaseTools",    # MU_CHANGE
+                "CryptoPkg",
+>>>>>>> d4d2ac9173 (BaseCore: Enable CI)
                 "MdePkg",
                 "MdeModulePkg",
                 "NetworkPkg",
                 "PcAtChipsetPkg",
+<<<<<<< HEAD
                 "SecurityPkg",
                 "UefiCpuPkg",
                 "FmpDevicePkg",
+=======
+>>>>>>> d4d2ac9173 (BaseCore: Enable CI)
                 "ShellPkg",
+                "UefiCpuPkg",
                 "StandaloneMmPkg",
+<<<<<<< HEAD
                 "FatPkg",
                 "CryptoPkg",
                 "PrmPkg",
@@ -92,6 +89,9 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
                 "SourceLevelDebugPkg",
                 "UefiPayloadPkg",
                 "TcgTpmPkg"
+=======
+                "UnitTestFrameworkPkg"
+>>>>>>> d4d2ac9173 (BaseCore: Enable CI)
                 )
 
     def GetArchitecturesSupported(self):
@@ -134,7 +134,7 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
         '''
         unsupported = set(list_of_requested_architectures) - \
             set(self.GetArchitecturesSupported())
-        if(len(unsupported) > 0):
+        if (len(unsupported) > 0):
             logging.critical(
                 "Unsupported Architecture Requested: " + " ".join(unsupported))
             raise Exception(
@@ -175,6 +175,11 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
                         "STUART_CODEQL_AUDIT_ONLY",
                         "TRUE",
                         "Set in CISettings.py")
+                    shell_environment.GetBuildVars().SetValue(
+                        "STUART_CODEQL_FILTER_FILES",
+                        os.path.join(self.GetWorkspaceRoot(),
+                                     "CodeQlFilters.yml"),
+                        "Set in CISettings.py")
             except NameError:
                 pass
 
@@ -208,14 +213,17 @@ class Settings(CiBuildSettingsManager, UpdateSettingsManager, SetupSettingsManag
             "MdePkg/Library/MipiSysTLib/mipisyst", False))
         rs.append(RequiredSubmodule(
             "CryptoPkg/Library/MbedTlsLib/mbedtls", False))
+<<<<<<< HEAD
         rs.append(RequiredSubmodule(
             "SecurityPkg/DeviceSecurity/SpdmLib/libspdm", False))
         rs.append(RequiredSubmodule(
             "TcgTpmPkg/Library/TpmLib/TPM", False))
+=======
+>>>>>>> d4d2ac9173 (BaseCore: Enable CI)
         return rs
 
     def GetName(self):
-        return "Edk2"
+        return "Basecore" # MU_CHANGE
 
     def GetDependencies(self):
         return [
