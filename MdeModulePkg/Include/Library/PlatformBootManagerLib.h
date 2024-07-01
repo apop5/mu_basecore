b273657f7e22f0903976d5c2e6185e513f710db1
Bret Barkelew <brbarkel@microsoft.com>
2024-07-01 13:46:11 -0700

    MdeModulePkg: Adds platforms specific actions required at the start of BDS
    Adds the following functions to the PlatformBootManagerLib:
     - PlatformBootManagerBdsEntry
     - PlatformBootManagerProcessBootCompletion
     - PlatformBootManagerPriorityBoot
     - PlatformBootManagerOnDemandConInConnect
    
    This change was split off the commit:
    https://github.com/microsoft/mu_basecore/commit/7a38833

--- MdeModulePkg/Include/Library/PlatformBootManagerLib.h
+++ MdeModulePkg/Include/Library/PlatformBootManagerLib.h
@@ -7,8 +7,8 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 **/
 
-#pragma once
-
+#ifndef __PLATFORM_BOOT_MANAGER_LIB_H_
+#define __PLATFORM_BOOT_MANAGER_LIB_H_
 #include <Library/UefiBootManagerLib.h>
 
 /**
@@ -64,3 +64,53 @@ EFIAPI
 PlatformBootManagerUnableToBoot (
   VOID
   );
+
+/**  MU_CHANGE [BEGIN]
+   Do Platform specific action required at start of BDS
+
+**/
+VOID
+EFIAPI
+PlatformBootManagerBdsEntry (
+  VOID
+  );
+
+/**
+ProcessBootCompletion
+*/
+VOID
+EFIAPI
+PlatformBootManagerProcessBootCompletion (
+  IN EFI_BOOT_MANAGER_LOAD_OPTION  *BootOption
+  );
+
+/**
+ HardKeyBoot
+*/
+VOID
+EFIAPI
+PlatformBootManagerPriorityBoot (
+  UINT16  **BootNext
+  );
+
+/**
+ BDS Deadloop - error, unable to boot any boot option
+*/
+VOID
+EFIAPI
+PlatformBootManagerDeadloop (
+  VOID
+  );
+
+/**
+  OnDemandConInCOnnect
+ */
+VOID
+EFIAPI
+PlatformBootManagerOnDemandConInConnect (
+  VOID
+  );
+
+/**  MU_CHANGE [END] */
+
+#endif
