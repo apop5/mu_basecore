f81af690eb7269a195be96d3fd5b222b08eb6c5b
kuqin12 <42554914+kuqin12@users.noreply.github.com>
2025-10-30 12:43:05 -0700

    Adding check for underflow/overflow when iterating through resource descriptor hobs (#1549)
    During DXE core memory service initialization, the system would check
    available resource descriptor hobs against the memory top from PHIT hob.
    
    However, it is possible that a given resource descriptor hob will not be
    larger than the cover the memory top, causing the Length calculation to
    underflow. Or the hob is not properly prepared and cause the calculation
    to overflow.
    
    This change adds a check for potential underflow/overflow before
    performing the subtraction.
    
    - [x] Impacts functionality?
    - [ ] Impacts security?
    - [ ] Breaking change?
    - [ ] Includes tests?
    - [ ] Includes documentation?
    
    This change was tested on QEMU SBSA and booted to both UEFI shell and
    Windows VOS.
    
    N/A

--- MdeModulePkg/Core/Dxe/Gcd/Gcd.c
+++ MdeModulePkg/Core/Dxe/Gcd/Gcd.c
@@ -161,10 +161,7 @@ CoreDumpGcdMemorySpaceMap (
   }
 
   Status = CoreGetMemorySpaceMap (&NumberOfDescriptors, &MemorySpaceMap);
-  if (!((Status == EFI_SUCCESS) && (MemorySpaceMap != NULL))) {
-    ASSERT ((Status == EFI_SUCCESS) && (MemorySpaceMap != NULL));
-    return;
-  }
+  ASSERT (Status == EFI_SUCCESS && MemorySpaceMap != NULL);
 
   if (InitialMap) {
     DEBUG ((DEBUG_GCD, "GCD:Initial GCD Memory Space Map\n"));
@@ -217,10 +214,7 @@ CoreDumpGcdIoSpaceMap (
   }
 
   Status = CoreGetIoSpaceMap (&NumberOfDescriptors, &IoSpaceMap);
-  if (!((Status == EFI_SUCCESS) && (IoSpaceMap != NULL))) {
-    ASSERT ((Status == EFI_SUCCESS) && (IoSpaceMap != NULL));
-    return;
-  }
+  ASSERT (Status == EFI_SUCCESS && IoSpaceMap != NULL);
 
   if (InitialMap) {
     DEBUG ((DEBUG_GCD, "GCD:Initial GCD I/O Space Map\n"));
@@ -776,14 +770,13 @@ CoreConvertSpace (
     Map = &mGcdIoSpaceMap;
   } else {
     ASSERT (FALSE);
-    return EFI_NOT_FOUND;
   }
 
   //
   // Search for the list of descriptors that cover the range BaseAddress to BaseAddress+Length
   //
   Status = CoreSearchGcdMapEntry (BaseAddress, Length, &StartLink, &EndLink, Map);
-  if (EFI_ERROR (Status) || ((StartLink == NULL) || (EndLink == NULL))) {
+  if (EFI_ERROR (Status)) {
     Status = EFI_UNSUPPORTED;
 
     goto Done;
@@ -1191,7 +1184,6 @@ CoreAllocateSpace (
     Map = &mGcdIoSpaceMap;
   } else {
     ASSERT (FALSE);
-    return EFI_NOT_FOUND;
   }
 
   Found     = FALSE;
@@ -1359,12 +1351,6 @@ CoreAllocateSpace (
   // Convert/Insert the list of descriptors from StartLink to EndLink
   //
   Link = StartLink;
-  if ((Link == NULL) || (EndLink == NULL)) {
-    ASSERT (Link != NULL);
-    ASSERT (EndLink != NULL);
-    goto Done;
-  }
-
   while (Link != EndLink->ForwardLink) {
     Entry = CR (Link, EFI_GCD_MAP_ENTRY, Link, EFI_GCD_MAP_SIGNATURE);
     CoreInsertGcdMapEntry (Link, Entry, *BaseAddress, Length, TopEntry, BottomEntry);
@@ -2288,7 +2274,7 @@ CoreInitializeMemoryServices (
   EFI_HOB_GUID_TYPE            *GuidHob;
   UINT32                       ReservedCodePageNumber;
   UINT64                       MinimalMemorySizeNeeded;
-  EFI_PHYSICAL_ADDRESS         ResourceHobMemoryTop;
+  EFI_PHYSICAL_ADDRESS         ResourceHobMemoryTop;  // MU_CHANGE
 
   //
   // Point at the first HOB.  This must be the PHIT HOB.
@@ -2410,9 +2396,7 @@ CoreInitializeMemoryServices (
       continue;
     }
 
-    //
-    // Check for potential overflow before addition
-    //
+    // MU_CHANGE START - Check for potential overflow before addition
     if (ResourceHob->PhysicalStart > MAX_UINT64 - ResourceHob->ResourceLength) {
       ASSERT (FALSE);
       ResourceHobMemoryTop = MAX_UINT64;
@@ -2421,6 +2405,7 @@ CoreInitializeMemoryServices (
     }
 
     if (PhitHob->EfiFreeMemoryTop > ResourceHobMemoryTop) {
+      // MU_CHANGE END - Check for potential overflow before addition
       continue;
     }
 
@@ -2445,12 +2430,15 @@ CoreInitializeMemoryServices (
     Attributes  = PhitResourceHob->ResourceAttribute;
     BaseAddress = PageAlignAddress (PhitHob->EfiMemoryTop);
 
+    // MU_CHANGE START - Check for potential underflow before subtraction
     if (BaseAddress > ResourceHobMemoryTop) {
       Length = 0;
     } else {
       Length = PageAlignLength (ResourceHobMemoryTop - BaseAddress);
       FindLargestFreeRegion (&BaseAddress, &Length, (EFI_HOB_MEMORY_ALLOCATION *)GetFirstHob (EFI_HOB_TYPE_MEMORY_ALLOCATION));
     }
+
+    // MU_CHANGE END - Check for potential underflow before subtraction
 
     if (Length < MinimalMemorySizeNeeded) {
       //
@@ -2527,9 +2515,7 @@ CoreInitializeMemoryServices (
         continue;
       }
 
-      //
-      // Check for potential overflow before addition
-      //
+      // MU_CHANGE START - Check for potential overflow before addition
       if (ResourceHob->PhysicalStart > MAX_UINT64 - ResourceHob->ResourceLength) {
         ASSERT (FALSE);
         ResourceHobMemoryTop = MAX_UINT64;
@@ -2538,6 +2524,7 @@ CoreInitializeMemoryServices (
       }
 
       if (ResourceHobMemoryTop > (EFI_PHYSICAL_ADDRESS)MAX_ALLOC_ADDRESS) {
+        // MU_CHANGE END - Check for potential overflow before addition
         continue;
       }
 
@@ -2552,7 +2539,7 @@ CoreInitializeMemoryServices (
       // Skip Resource Descriptor HOBs that are not large enough to initilize the DXE Core
       //
       TestedMemoryBaseAddress = PageAlignAddress (ResourceHob->PhysicalStart);
-      TestedMemoryLength      = PageAlignLength (ResourceHobMemoryTop - TestedMemoryBaseAddress);
+      TestedMemoryLength      = PageAlignLength (ResourceHobMemoryTop - TestedMemoryBaseAddress); // MU_CHANGE
       FindLargestFreeRegion (&TestedMemoryBaseAddress, &TestedMemoryLength, (EFI_HOB_MEMORY_ALLOCATION *)GetFirstHob (EFI_HOB_TYPE_MEMORY_ALLOCATION));
       if (TestedMemoryLength < MinimalMemorySizeNeeded) {
         continue;
@@ -2665,11 +2652,7 @@ CoreInitializeGcdServices (
   // Get the number of address lines in the I/O and Memory space for the CPU
   //
   CpuHob = GetFirstHob (EFI_HOB_TYPE_CPU);
-  if (CpuHob == NULL) {
-    ASSERT (CpuHob != NULL);
-    return EFI_OUT_OF_RESOURCES;
-  }
-
+  ASSERT (CpuHob != NULL);
   SizeOfMemorySpace = CpuHob->SizeOfMemorySpace;
   SizeOfIoSpace     = CpuHob->SizeOfIoSpace;
 
@@ -2677,10 +2660,7 @@ CoreInitializeGcdServices (
   // Initialize the GCD Memory Space Map
   //
   Entry = AllocateCopyPool (sizeof (EFI_GCD_MAP_ENTRY), &mGcdMemorySpaceMapEntryTemplate);
-  if (Entry == NULL) {
-    ASSERT (Entry != NULL);
-    return EFI_OUT_OF_RESOURCES;
-  }
+  ASSERT (Entry != NULL);
 
   Entry->EndAddress = LShiftU64 (1, SizeOfMemorySpace) - 1;
 
@@ -2692,10 +2672,7 @@ CoreInitializeGcdServices (
   // Initialize the GCD I/O Space Map
   //
   Entry = AllocateCopyPool (sizeof (EFI_GCD_MAP_ENTRY), &mGcdIoSpaceMapEntryTemplate);
-  if (Entry == NULL) {
-    ASSERT (Entry != NULL);
-    return EFI_OUT_OF_RESOURCES;
-  }
+  ASSERT (Entry != NULL);
 
   Entry->EndAddress = LShiftU64 (1, SizeOfIoSpace) - 1;
 
@@ -2921,10 +2898,7 @@ CoreInitializeGcdServices (
                  (UINTN)PhitHob->EfiFreeMemoryBottom - (UINTN)(*HobStart),
                  *HobStart
                  );
-  if (NewHobList == NULL) {
-    ASSERT (NewHobList != NULL);
-    return EFI_OUT_OF_RESOURCES;
-  }
+  ASSERT (NewHobList != NULL);
 
   *HobStart = NewHobList;
   gHobList  = NewHobList;
